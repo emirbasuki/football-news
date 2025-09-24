18f2506408f35794a6a7f63b25cbd6500187dc47
Emir Fadhil Basuki <emir.fadhil41@ui.ac.id>
2025-09-24 13:48:43 +0700

    Tutorial 4

--- football_news/settings.py
+++ football_news/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "emir-fadhil41-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://pbp.cs.ui.ac.id/emir.fadhil41/footballnews"
+    "https://emir-fadhil41-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
